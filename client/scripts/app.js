6cdb3b8bd7b4e1956e0a9dac95d7125178a04181
cwillens <charwillens@gmail.com>
2016-09-26 12:24:09 -0700

    Implement app.fetch() and app.send().

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,55 @@
 // YOUR CODE HERE:
+var app = {};
+
+var entityMap = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  "'": '&#39;',
+  '/': '&#x2F;'
+};
+
+var escapeHTML = function(string) {
+  return String(string).replace(/[&<>"'\/]/g, function (s) {
+    return entityMap[s];
+  });
+};
+
+app.init = function(name) {
+  this.name = name;
+  this.roomname = 'lobby';
+};
+
+app.send = function(message) {
+  var messageObj = {
+    username: this.name,
+    text: message,
+    roomname: this.roomname
+  };
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(messageObj),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.fetch = function() {
+
+  $.get('https://api.parse.com/1/classes/messages', function(data) {
+    data.results.forEach(function(elem) {
+      var $newNode = $('<div id="' + elem.createdAt + '" class="' + elem.roomname + ' ' + escapeHTML(elem.username) + '">' + escapeHTML(elem.username) + ': ' + escapeHTML(elem.text) + '</div>');
+      //console.log($newNode);
+      $('#chats').append($newNode);
+    });
+  });
+};
